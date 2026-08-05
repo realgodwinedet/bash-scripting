a76792b293eaf85c683d909dfec28dab500b26eb
Ikpi Godwin Edet <realgodwinedet@gmail.com>
2026-08-05 11:45:05 +0100

    Create 14_file_permissions.sh

---
+++ 14_file_permissions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter afilename:"
+read filename
+if [ -r "$filename" ]; then
+    echo "$filename is readable."
+fi
+if [ -w "$filename" ]; then
+    echo "$filename is writable."
+fi
+if [ -x "$filename" ]; then
+    echo "$filename is executable."
+fi
