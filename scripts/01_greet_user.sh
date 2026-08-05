82169c3fde256299103cd52b30874fdfcc41fd76
Ikpi Godwin Edet <realgodwinedet@gmail.com>
2026-08-05 10:04:01 +0100

    Create 01_greet_user.sh
    Add greeting script

---
+++ scripts/01_greet_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ====================================
+# Script: Greeting the User
+# Description: Prompts the user for their name and displays a greeting.
+# ====================================
+
+echo "Enter your name:"
+read name
+
+echo "Hello, $name! Welcome to Bash scripting.
